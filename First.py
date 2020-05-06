75652b6a1ca0da2a991d02dc1a1741248d732e37
Karthi Vignesh <46532955+k4rth1v1gn3sh@users.noreply.github.com>
2020-05-06 13:12:46 +0530

    Adding name for easing conversation
    The users can now enter the name or their identity so that the one who is receiving it can understand whom they are communicating with

--- First.py
+++ First.py
@@ -2,14 +2,15 @@ from threading import Thread
 import socket as sk
 
 class sender(Thread):
-    def __init__(self, msg):
+    def __init__(self, msg, name):
         Thread.__init__(self)
         self.msg = msg
+        self.name = name
 
     def run(self):
         while True:
             x = input()
-            x = 'Lucifer : ' + x
+            x = self.name + ' : ' + x
             self.msg.send(bytes(x, 'utf-8'))
 
 class reciever(Thread):
@@ -22,13 +23,13 @@ class reciever(Thread):
             x = self.msg.recv(100)
             print(x.decode('utf-8'))
 
-
+name = input("Enter your name ")
 c = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 c.bind(('127.0.0.1', 4096))
 c.listen()
 msg, address = c.accept()
 
-s1 = sender(msg)
+s1 = sender(msg, name)
 r1 = reciever(msg)
 
 s1.start()
