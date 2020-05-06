871bdddbd2c2136d49a397558cd151afc85c30d1
Karthi Vignesh <46532955+k4rth1v1gn3sh@users.noreply.github.com>
2020-05-06 13:14:48 +0530

    Updating name for easing the conversation
    The users can now enter the name or their identity so that the one who is receiving it can understand whom they are communicating with

--- Second.py
+++ Second.py
@@ -2,14 +2,15 @@ from threading import Thread
 import socket as sk
 
 class sender(Thread):
-    def __init__(self, msg):
+    def __init__(self, msg, name):
         Thread.__init__(self)
         self.msg = msg
+        self.name = name
 
     def run(self):
         while True:
             x = input()
-            x = 'Karthi : ' + x
+            x = self.name + ' : ' + x
             self.msg.send(bytes(x, 'utf-8'))
 
 class reciever(Thread):
@@ -22,10 +23,11 @@ class reciever(Thread):
             x = self.msg.recv(100)
             print(x.decode('utf-8'))
 
+name = input("Enter your name ")
 c = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 c.connect(('127.0.0.1', 4096))
 
-s1 = sender(c)
+s1 = sender(c, name)
 r1 = reciever(c)
 
 s1.start()
